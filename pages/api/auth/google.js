d12752f79cf57419696c5d3d407369e5ebaee180
Dani <danielmunozgil@gmail.com>
2023-04-10 21:33:26 +0200

    trying NextJS API Routes, again - https://vercel.com/guides/using-express-with-vercel

--- pages/api/auth/google.js
+++
@@ -1,6 +0,0 @@
-// Redirect the user to the Google signin page
-//app.get("/api/auth/google", passport.authenticate("google", { scope: ["email", "profile"] }));
-export default function handler(req, res) {
-    passport.authenticate("google", { scope: ["email", "profile"] });
-}
-
